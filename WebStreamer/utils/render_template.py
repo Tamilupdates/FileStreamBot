4d3b4c9787abc546646178b13c60f1ac0a501ce6
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-05-28 06:54:32 +0530

    latest

--- WebStreamer/utils/render_template.py
+++ WebStreamer/utils/render_template.py
@@ -7,7 +7,7 @@ from WebStreamer.vars import Var
 from WebStreamer.utils.database import Database
 from WebStreamer.utils.human_readable import humanbytes
 db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
-
+'''
 async def render_page(db_id):
     file_data=await db.get_file(db_id)
     src = urllib.parse.urljoin(Var.URL, f'dl/{file_data["_id"]}')
@@ -29,3 +29,27 @@ async def render_page(db_id):
                     file_size = humanbytes(int(u.headers.get('Content-Length')))
                     html = (await r.read()) % (heading, file_data['file_name'], src, file_size)
     return html
+'''
+
+async def render_page(db_id):
+    file_data=await db.get_file(db_id)
+    src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
+    file_size = humanbytes(file_data['file_size'])
+    file_name = file_data['file_name'].replace("_", " ")
+
+    if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
+        template_file = "FileStream/template/play.html"
+    else:
+        template_file = "FileStream/template/dl.html"
+        async with aiohttp.ClientSession() as s:
+            async with s.get(src) as u:
+                file_size = humanbytes(int(u.headers.get('Content-Length')))
+
+    with open(template_file) as f:
+        template = jinja2.Template(f.read())
+
+    return template.render(
+        file_name=file_name,
+        file_url=src,
+        file_size=file_size
+    )
